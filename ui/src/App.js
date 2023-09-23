d435347ba9fd5acdd51687c635dc85e4b1fafa03
Stanislau Matskevich <st.matskevich@gmail.com>
2023-09-23 21:28:36 +0200

    Added telegram-web-app library

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,21 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onShowTelegramDialog = () => {
+    window.Telegram.WebApp.showAlert("Message from Telegram Mini App Template Bot");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to Telegram Mini App Template Bot</p>
+        <div className="button" onClick={onShowTelegramDialog}>Show Telegram Dialog</div>
       </header>
     </div>
   );
