911659d6eea9545f8ca732a83e1bf37243a29593
Stanislau Matskevich <st.matskevich@gmail.com>
2023-09-25 21:59:50 +0200

    Refactored and added comments to API service

--- api/main.go
+++ api/main.go
@@ -9,19 +9,32 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2"
 )
 
+const SOURCE_CODE_URL = "https://github.com/st-matskevich/tg-mini-app-template/"
+
 func main() {
 	log.Println("Starting API service")
 
-	sourceCodeURL := "https://github.com/st-matskevich/tg-mini-app-template/"
 	webAppURL := os.Getenv("TELEGRAM_WEB_APP_URL")
 	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
-	botClient, err := gotgbot.NewBot(botToken, nil)
+	bot, err := gotgbot.NewBot(botToken, nil)
 	if err != nil {
 		log.Fatalf("Telegram Bot API initialization error: %v", err)
 	}
 	log.Println("Telegram Bot API initialized")
 
-	http.HandleFunc("/bot", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/bot", CreateBotEndpointHandler(bot, webAppURL))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// According to the https://core.telegram.org/bots/api#setwebhook webhook will receive JSON-serialized Update structure
+// Handler created by this function parses Update structure and replies to any message with welcome text and inline keyboard to open Mini App
+func CreateBotEndpointHandler(bot *gotgbot.Bot, appURL string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Serving %s route", r.URL.Path)
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusNotImplemented)
@@ -44,24 +57,18 @@ func main() {
 		opts := &gotgbot.SendMessageOpts{
 			ReplyMarkup: gotgbot.InlineKeyboardMarkup{
 				InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
-					{Text: "Open mini app", WebApp: &gotgbot.WebAppInfo{Url: webAppURL}},
+					{Text: "Open mini app", WebApp: &gotgbot.WebAppInfo{Url: appURL}},
 				}, {
-					{Text: "Open source code", Url: sourceCodeURL},
+					{Text: "Open source code", Url: SOURCE_CODE_URL},
 				}},
 			},
 		}
 
-		if _, err := botClient.SendMessage(update.Message.Chat.Id, message, opts); err != nil {
+		if _, err := bot.SendMessage(update.Message.Chat.Id, message, opts); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		w.WriteHeader(http.StatusOK)
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
 	}
-	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
